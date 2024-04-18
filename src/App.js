a9c13e8a454ecb2a2a160dc2f342c77e355b8687
unknown <santiago.rodriguez-s@labinfo.is.escuelaing.edu.co>
2024-04-18 14:37:18 -0500

    Iniciando Landing Page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ShowLandingPage from './components/ShowLandingPage';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Armario virtual</h1>
+      <ShowLandingPage />
+    </div>
+  );
+}
+
+
+export default App;
+
---
+++ src/components/ShowLandingPage.js
@@ -0,0 +1,72 @@
+import React from 'react';
+// import './App.css'; // Estilos
+import logo from './logo1.png';
+import step from './logo2.png';
+function App() {
+  return (
+    <div className="App"><img src = {logo} alt="Logo de la aplicación" />
+      <header>
+       
+        <nav>
+          <ul>
+            <li><a href="#inicio">Inicio</a></li>
+            <li><a href="#como-funciona">Cómo Funciona</a></li>
+            <li><a href="#caracteristicas">Características</a></li>
+            <li><a href="#testimonios">Testimonios</a></li>
+            <li><a href="#contacto">Contacto</a></li>
+          </ul>
+        </nav>
+      </header>
+      <section id="inicio" className="hero-section">
+        <div className="hero-content">
+          <h1>Organiza tu armario virtualmente</h1>
+          <p>Mantén tu guardarropa organizado y a mano en todo momento.</p>
+        </div>
+      </section>
+      <section id="como-funciona" className="how-it-works-section">
+        <h2>Cómo Funciona</h2>
+        <div className="steps">
+          <div className="step">
+            <img src={step} alt="Paso 1" />
+            <p>Fotografía tus prendas</p>
+          </div>
+          <div className="step">
+            <img src={step} alt="Paso 2" />
+            <p>Organízalas por categorías</p>
+          </div>
+          <div className="step">
+            <img src={step} alt="Paso 3" />
+            <p>Crea conjuntos fácilmente</p>
+          </div>
+        </div>
+      </section>
+      <section id="caracteristicas" className="features-section">
+        <h2>Características</h2>
+        <ul>
+          <li>
+            <h3>Gestión de guardarropas</h3>
+            <p>Administra todas tus prendas de manera eficiente.</p>
+          </li>
+          <li>
+            <h3>Creación de conjuntos</h3>
+            <p>Crea combinaciones de ropa fácilmente.</p>
+          </li>
+          <li>
+            <h3>Sugerencias de combinaciones</h3>
+            <p>Recibe sugerencias de conjuntos basadas en tus prendas.</p>
+          </li>
+          <li>
+            <h3>Calendario de outfits</h3>
+            <p>Planifica qué ponerte con anticipación.</p>
+          </li>
+        </ul>
+      </section>
+      {/* Sección de Testimonios */}
+      {/* Sección de Contacto */}
+      {/* Pie de página */}
+      <script src="app.js"></script>
+    </div>
+  );
+}
+
+export default App;
