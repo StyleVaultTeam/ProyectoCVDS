4be266854197eee7d7a8ad8f19da003adce0300d
JuanDpr99 <juan.parroquiano@mail.escuelaing.edu.co>
2024-04-19 00:36:43 -0500

    aporte 1 proyecto

---
+++ src/components/ImagesClothes/camisetas.js
@@ -0,0 +1,7 @@
+import image1 from "../ImagesClothes/Camiseta_3_Rayas_adidas_Essentials_Azul.png";
+import image2 from "../ImagesClothes/Camiseta_Rosa_.png";
+import image3 from "../ImagesClothes/CamisetaNegra.png";
+import image4 from "../ImagesClothes/Camiseta_Blanca.png"; 
+import image5 from "../ImagesClothes/camiseta-azul.png";
+
+export default [image1, image2, image3, image4, image5]
---
+++ src/components/ImagesClothes/pantalones.js
@@ -0,0 +1,5 @@
+import image1 from "../ImagesClothes/Pantalon_Adicolor_Classics.png";
+import image2 from "../ImagesClothes/Pantalon_Adicolor.png";
+import image3 from "../ImagesClothes/Pantalon_Train.png";
+
+export default [image1, image2, image3]
