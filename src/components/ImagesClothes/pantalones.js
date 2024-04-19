ec420fcc64ab456f6ea01950d59de41e112315e7
Linkcucus <124446857+koket987@users.noreply.github.com>
2024-04-19 12:17:46 -0500

    Update pantalones.js

--- src/components/ImagesClothes/pantalones.js
+++ src/components/ImagesClothes/pantalones.js
@@ -2,4 +2,6 @@ import image1 from "../ImagesClothes/Pantalon_Adicolor_Classics.png";
 import image2 from "../ImagesClothes/Pantalon_Adicolor.png";
 import image3 from "../ImagesClothes/Pantalon_Train.png";
 
-export default [image1, image2, image3]
+const pantalonesImages = [image1, image2, image3];
+
+export default pantalonesImages;
