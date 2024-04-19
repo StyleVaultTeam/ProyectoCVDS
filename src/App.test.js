b8588ed99cb66eb2ec9eb288a947aa1546f40b35
Koket987 <santiago.rodriguez-s@labinfo.is.escuelaing.edu.co>
2024-04-19 13:25:04 -0500

    Iniciando Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import { render,fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header with correct text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerText = screen.getByText(/organiza tu armario virtualmente/i);
+  expect(headerText).toBeInTheDocument();
+});
+test('renders application logo', () => {
+  render(<App />);
+  const logoElement = screen.getByAltText('Logo de la aplicación');
+  expect(logoElement).toBeInTheDocument();
+});
+test('renders navigation link for "Inicio"', () => {
+  render(<App />);
+  const inicioLink = screen.getByText('Inicio');
+  expect(inicioLink).toBeInTheDocument();
+});
+test('does not navigate when "Inicio" link is clicked', () => {
+  // Renderizamos el componente
+  render(<App />);
+
+  // Encontramos el enlace de "Inicio"
+  const inicioLink = screen.getByText('Inicio');
+
+  // Simulamos un clic en el enlace de "Inicio"
+  fireEvent.click(inicioLink);
+
+  // Verificamos que la sección "Inicio" no esté presente después del clic
+  const inicioSection = screen.queryByTestId('inicio-section');
+  expect(inicioSection).toBeNull();
 });
