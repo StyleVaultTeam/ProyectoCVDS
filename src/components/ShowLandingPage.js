47b15d778f0af3d57a53085ffeaccdd50bfa1d57
Nicolas <nicolas.bernal-f@mail.escuelaing.edu.co>
2024-04-22 17:29:23 -0500

    Pie de pagina

--- src/components/ShowLandingPage.js
+++ src/components/ShowLandingPage.js
@@ -19,7 +19,6 @@ function App() {
             <li><a href="#como-funciona">Cómo Funciona</a></li>
             <li><a href="#caracteristicas">Características</a></li>
             <li><a href="#testimonios">Testimonios</a></li>
-            <li><a href="#contacto">Contacto</a></li>
           </ul>
         </nav>
       </header>
@@ -77,8 +76,32 @@ function App() {
         </ul>
       </section>
       {/* Sección de Testimonios */}
-      {/* Sección de Contacto */}
       {/* Pie de página */}
+      <footer className="footer">
+        <div className="container">
+          <div className="footer-content">
+            <div className="footer-section about">
+              <h2>Sobre Nosotros</h2>
+              <p>Grupo de estudiantes de la carrera de Ingenieria de Sistemas de la Escuela Colombiana de Ingenieria.</p>
+            </div>
+            <div className="footer-section contact">
+              <h2>Contacto</h2>
+              <div className="contact-info">
+                <p><i className="fas fa-envelope"></i> styleVault@mail.com</p>
+                <p><i className="fas fa-phone"></i> +3143692005</p>
+              </div>
+              <div className="socials">
+                <a href="#"><i className="fab fa-facebook"></i></a>
+                <a href="#"><i className="fab fa-twitter"></i></a>
+                <a href="#"><i className="fab fa-instagram"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          &copy; 2024 StyleVault | Todos los derechos reservados
+        </div>
+      </footer>
       <script src="app.js"></script>
     </div>
   );
