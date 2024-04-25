b1a9a0dee0efaa2eaa939e17d459c7a81e4ece99
JuanDpr99 <juan.parroquiano@mail.escuelaing.edu.co>
2024-04-25 01:47:15 -0500

    ajuste adaptabilidad a diferentes dispositivos (corregido2)

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -5,12 +5,10 @@ import { motion } from "framer-motion";
 function Slider({ arregloImagenes }) {
     return (
         <motion.div className="slider-container">
-            {/*<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css" integrity="sha384-BY+fdrpOd3gfeRvTSMT+VUZmA728cfF9Z2G42xpaRkUGu2i3DyzpTURDo5A6CaLK" crossorigin="anonymous" />
-            */}
             <motion.div className="slider" drag='x' dragConstraints={{right: 250*(arregloImagenes.length/2), left: -250*(arregloImagenes.length/2) }} >
                 {arregloImagenes.map((image, index) => (
-                    <motion.div class="card" className="item" style="" key={index}>
-                        <img src={image} class="card-img-top" alt=""></img>
+                    <motion.div class="card" className="item" style={{width: "18rem"}} key={index}>
+                        <img src={image} class="img-fluid" alt=""></img>
                         <div class="rating">
                             <input type="radio" name="star" id="star1" /><label for="star1"></label>
                             <input type="radio" name="star" id="star2" /><label for="star2"></label>
