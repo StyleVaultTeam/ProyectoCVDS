8d4e087470c9726537dd9c0daa2ae8d3bf171161
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2024-04-26 20:18:28 -0500

    Forzar el despliegue en mi rama llamada feature/add-routing

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import OutfitsStylesReferences from './components/OutfitsStylesReferences/Outfit
 import NotFound from './components/NotFound/NotFound'
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('landing');
-
   return (
     <BrowserRouter>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous"></link>
