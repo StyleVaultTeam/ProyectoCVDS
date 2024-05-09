5e5182f9588db4c51df3ccf0dde212887c902bcb
koket987 <you@example.com>
2024-05-09 13:31:56 -0500

    Añadiendo cookie login

--- src/components/LoginForm/Login.jsx
+++ src/components/LoginForm/Login.jsx
@@ -1,25 +1,61 @@
 import './Login.css';
 import { FaUser, FaLock } from "react-icons/fa";
-
+import { useState } from "react";
 
 const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        const data = { email, password };
+
+        try {
+            const response = await fetch('https://appcvds2.azurewebsites.net/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                const { token } = await response.json();
+                document.cookie = `authToken=${token}`;
+            } else {
+                if (response.status === 403) {
+                    setErrorMessage('CONTRASEÑA INCORRECTA');
+                } else if (response.status === 404) {
+                    setErrorMessage('USUARIO INCORRECTO');
+                } else {
+                    setErrorMessage('Error desconocido');
+                }
+            }
+        } catch (error) {
+            console.error('Error al enviar la solicitud:', error);
+            setErrorMessage('Error de red');
+        }
+    };
+
     return(
         <div className='wrapper'>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <h1>Login</h1>
+                {errorMessage && <div className="error">{errorMessage}</div>}
                 <div className="input-box">
-                    <input type="text" placeholder="Usuario" required />
+                    <input type="text" placeholder="Usuario" required value={email} onChange={(e) => setEmail(e.target.value)} />
                     <FaUser className='icon'/>
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder="Contraseña" required />
+                    <input type="password" placeholder="Contraseña" required value={password} onChange={(e) => setPassword(e.target.value)} />
                     <FaLock className='icon'/>
                 </div>
-                <button type="sumbit">Iniciar Sesion</button>
+                <button type="submit">Iniciar Sesión</button>
             </form>
         </div>
     );
 };
 
 export default LoginForm;
-
