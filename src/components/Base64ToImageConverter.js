487840a16fe68931c17a249b576d80605d871155
unknown <santiago.rodriguez-s@labinfo.is.escuelaing.edu.co>
2024-05-15 12:24:18 -0500

    actualizando main

--- src/components/Base64ToImageConverter.js
+++ src/components/Base64ToImageConverter.js
@@ -9,18 +9,6 @@ const Base64ToImageConverter = () => {
     return img;
   };
   
-  const handleConvert = () => {
-    const imgElement = getFileFromBase64(base64String);
-    document.body.appendChild(imgElement);
-  };
-
-  return (
-    <div>
-      <h1>Convertir Base64 a Imagen</h1>
-      <textarea id="payload" value={base64String} onChange={(e) => setBase64String(e.target.value)} />
-      <button id="convert" onClick={handleConvert}>Convertir</button>
-    </div>
-  );
-};
+}
 
 export default Base64ToImageConverter;
